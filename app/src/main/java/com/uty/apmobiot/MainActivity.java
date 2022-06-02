6c46ff9ed027d7fb2afbaee69312d3e604c9c370
YapZanan <asdqwe123@@A123>
2022-06-02 14:52:39 +0700

    penambahan comment

--- app/src/main/java/com/uty/apmobiot/MainActivity.java
+++ app/src/main/java/com/uty/apmobiot/MainActivity.java
@@ -76,115 +76,120 @@ public class MainActivity extends AppCompatActivity {
         DAOkondisiLampu daoKondisiLampu = new DAOkondisiLampu();
         DAOkecerahanLampu daoKecerahanLampu = new DAOkecerahanLampu();
 
-        loadData();
-        //pertama kali si lampu bakalan mati
-        kondisi(state);
-        seekbarLampu.setProgress(nilai);
-        ambilDataKecerahan();
-        ambilDataKondisi();
+        loadData(); //Akan load data dengan shared preferences
+        kondisi(state); //Setelah data diload, akan menerapkan data tersebut
+        seekbarLampu.setProgress(nilai); //menerapkan nilai seekbar
+        ambilDataKecerahan(); //mengambil data kecerahan dari firebase
+        ambilDataKondisi();//mengambil data kondisi dari firebase
 
 
-
+        //listener ketika seekbar ditekan
         seekbarLampu.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-                                                    @Override
-                                                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                                                    }
+        }
 
-                                                    @Override
-                                                    public void onStartTrackingTouch(SeekBar seekBar) {
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
 
-                                                    }
-
-                                                    @Override
-                                                    public void onStopTrackingTouch(SeekBar seekBar) {
-                                                        int kecerahan = seekBar.getProgress();
-                                                        pushKecerahanLampu(daoKecerahanLampu, dataKecerahanLampu(kecerahan));
-                                                        kecerahan(kecerahan);
-                                                    }
-                                                }
-        );
+        }
 
 
+        //listener ketika seekbar dilepaskan
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            int kecerahan = seekBar.getProgress(); //akan mengambil value dari seekbar
+            pushKecerahanLampu(daoKecerahanLampu, dataKecerahanLampu(kecerahan)); //mengirimkan data ke firebase
+            kecerahan(kecerahan); //mengatur kecerahan lampu dari data kecerahan yang sudah diambil sebelumnya
+        }
+        }
+    );
 
+        //listener button
         button.setOnClickListener(v -> {
-            state = button.isSelected();
-            pushKondisiLampu(daoKondisiLampu, dataKondisiLampu(state));
-            kondisi(state);
+            state = button.isSelected(); //mencari tau kondisi button
+            pushKondisiLampu(daoKondisiLampu, dataKondisiLampu(state)); //mengirimkan data button ke firebase
+            kondisi(state); //membalikkan posisi button dan seekbar
         });
     }
 
+
+    //function untuk menympan kondisi button dengan shared preferences
     private void saveDataKondisi(Boolean state) {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(buttonLampu, state);
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE); //membuat sharedpref dengan mode private
+        SharedPreferences.Editor editor = sharedPreferences.edit(); //memberikan hak edit ke shared pref
+        editor.putBoolean(buttonLampu, state); //menyimpan kondisi boolean ke shared pref
         Log.d("simpan", "Berhasil simpan");
         Log.d("simpan", Boolean.toString(state));
-        editor.apply();
+        editor.apply(); //apply shared pref
     }
 
+    //function untuk menympan data kecerahan dengan shared preferences
     private void saveDataKecerahan(int kecerahan) {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(nilaiLampu, kecerahan);
+        editor.putInt(nilaiLampu, kecerahan); //menyimpan data kecerahan ke shared pref
         Log.d("simpan", "Berhasil simpan");
         Log.d("simpan", Integer.toString(kecerahan));
         editor.apply();
     }
 
+    //akan dipanggil ketika aplikasi pertama dijalankan, memanggil shared pref yang sudah tersimpan
     private void loadData(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        state = sharedPreferences.getBoolean(buttonLampu, false);
-        nilai = sharedPreferences.getInt(nilaiLampu, 0);
-        Log.d("load", Boolean.toString(state));
+        state = sharedPreferences.getBoolean(buttonLampu, false); //ketika aplikasi pertama dijalankan, defalt akan ke false
+        nilai = sharedPreferences.getInt(nilaiLampu, 0); //ketika aplikasi pertama dijalankan, default kecerahan akan ke 0
     }
 
+    //fungsi untuk mengatur seekbar kecerahan
     private void kecerahan(int kecerahan){
-        seekbarLampu.setProgress(kecerahan);
-        saveDataKecerahan(kecerahan);
-        Log.d("onProgressChanged", Integer.toString(kecerahan));
-
+        seekbarLampu.setProgress(kecerahan); //mengatur kecerahan lampu
+        saveDataKecerahan(kecerahan); //save kecerahan lampu ke shared pref
     }
 
-
+    //fungsi ketika button ditekan
     private void kondisi(Boolean state){
 
-        if(state){
-            Log.d("aaa", "bb");
-            DrawableCompat.setTint(draw, Color.BLUE);
-            textStatus.setText("Nyala");
-            DrawableCompat.setTint(drawBackground, ContextCompat.getColor(this, R.color.teal_700));
-            seekbarLampu.setProgressTintList(null);
-            seekbarLampu.setThumbTintList(null);
+        if(state){ //jika true
+            DrawableCompat.setTint(draw, Color.BLUE); //set warna button ke warna yang sudah ditentukan
+            textStatus.setText("Nyala");//set text view
+            DrawableCompat.setTint(drawBackground, ContextCompat.getColor(this, R.color.teal_700)); //set warna halaman ke warna yang sudah ditentukan
+            seekbarLampu.setProgressTintList(null); //mengatur warna seekbar ke null
+            seekbarLampu.setThumbTintList(null); //mengatur warna thum ke null
         }
         else{
-            Log.d("cccc", "ddd");
             DrawableCompat.setTint(draw, Color.GRAY);
             textStatus.setText("Mati");
             DrawableCompat.setTint(drawBackground, ContextCompat.getColor(this, R.color.TurnOff));
             seekbarLampu.setProgressTintList(ColorStateList.valueOf(Color.GRAY));
             seekbarLampu.setThumbTintList(ColorStateList.valueOf(Color.DKGRAY));
         }
-        saveDataKondisi(state);
-        button.setSelected(!state);
-        seekbarLampu.setEnabled(state);
+        saveDataKondisi(state); //memanggil fungsi save kondisi
+        button.setSelected(!state); //mengatur state button agar menjadi sebaliknya
+        seekbarLampu.setEnabled(state); //mengatur state seekbar menjadi sesuai state
     }
 
+    //memgambil kondisi lampu
     private kondisiLampu dataKondisiLampu(Boolean state){
         return new kondisiLampu(state);
     }
-
+    //mengambil kondisi kecerahan
+    private kecerahanLampu dataKecerahanLampu(int kecerahan){
+        return new kecerahanLampu(kecerahan);
+    }
+    //fungsi untuk memanggil dao kondisi lampu
     private void pushKondisiLampu(DAOkondisiLampu dao, kondisiLampu kond){
+        //memanggil fungsi add pada dao
         dao.add(kond).addOnSuccessListener(
+                //ketika sukses
                         suc -> Toast.makeText(this, "Berhasil masukkan Data kondisi", Toast.LENGTH_SHORT).show())
+                //ketika gagal
                 .addOnFailureListener(
                         fail -> Toast.makeText(this, "Gagal masukkan Data kondisi", Toast.LENGTH_SHORT).show());
     }
 
-    private kecerahanLampu dataKecerahanLampu(int kecerahan){
-        return new kecerahanLampu(kecerahan);
-    }
-
+   //fungsi untuk memanggil dao kondisi kecerahan lampu
     private void pushKecerahanLampu(DAOkecerahanLampu dao, kecerahanLampu kecer){
         dao.add(kecer).addOnSuccessListener(
                         suc -> Toast.makeText(this, "Berhasil masukkan Data kecerahan", Toast.LENGTH_SHORT).show())
@@ -192,6 +197,7 @@ public class MainActivity extends AppCompatActivity {
                         fail -> Toast.makeText(this, "Gagal masukkan Data kecerahan", Toast.LENGTH_SHORT).show());
     }
 
+   //mengambil data kecerahan ketika runtime
     private void ambilDataKecerahan(){
         dref = FirebaseDatabase.getInstance().getReference();
         dref.addValueEventListener(new ValueEventListener() {
@@ -210,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //mengambil data kondisi ketika runtime
     private void ambilDataKondisi() {
         dref = FirebaseDatabase.getInstance().getReference();
         dref.addValueEventListener(new ValueEventListener() {
